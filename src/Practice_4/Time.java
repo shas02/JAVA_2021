f5a4f59392344fa58d90fe7dd6cb0eb63860ab93
shas02 <shas02qazxsw@gmail.com>
2021-07-22 22:43:10 +0300

    Made practice 4 (use of TreeSet and Map) and 5 (streem)

---
+++ src/Practice_4/Time.java
@@ -0,0 +1,52 @@
+package Practice_4;
+
+public class Time {
+    private int min;
+    private int hours;
+
+    public Time() {
+    }
+
+    public Time(int hours, int min) {
+        if (min < 0 || min > 59) {
+            if (hours < 0 || hours > 23) {
+                System.out.println("Wrong input");
+                return;
+            }
+        }
+        this.min = min;
+        this.hours = hours;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        if (min < 0 || min > 59) {
+            System.out.println("Wrong input");
+            return;
+        }
+        this.min = min;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        if (hours < 0 || hours > 23) {
+            System.out.println("Wrong input");
+            return;
+        }
+        this.hours = hours;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" +
+                "min=" + min +
+                ", hours=" + hours +
+                '}';
+    }
+}
